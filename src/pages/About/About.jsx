ca0e51e7282e67da61d73c49eb01f35b3fea9aba
Tiodevs <Tiodevs>
2023-06-21 13:59:21 -0300

    Testando deploy no Netlify

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,12 +1,11 @@
 import Nav from "../../components/nav/nav";
-import style from './About.css'
 
 export default function About() {
     return (
         <>
             <Nav />
             <div className="conteudo">
-                <h1>about</h1>
+                <h1>Essa pagina é provisoria :D</h1>
             </div>
         </>
     )
